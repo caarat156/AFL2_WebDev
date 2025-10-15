da0a3ad69665727e2b61e6b363af2258536f6bdf
caarat156 <amaylinda@student.ciputra.ac.id>
2025-10-15 18:17:28 +0700

    footer

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="bg-dark text-light py-5">
+<footer style="background-color: #f6efe8; color: #6b4e3d;" class="py-5">
     <div class="container">
         <div class="row align-items-center mb-4">
             <div class="col-md-4 text-center text-md-start mb-3 mb-md-0">
@@ -10,16 +10,16 @@
                 <div class="row">
                     <div class="col-md-6 text-center text-md-end">
                         <ul class="list-unstyled small mb-0">
-                            <li><a href="#" class="text-light text-decoration-none">INSTAGRAM</a></li>
-                            <li><a href="#" class="text-light text-decoration-none">SHOPEE</a></li>
-                            <li><a href="#" class="text-light text-decoration-none">TOKOPEDIA</a></li>
+                            <li><a href="#" style="color: #6b4e3d; text-decoration: none;">INSTAGRAM</a></li>
+                            <li><a href="#" style="color: #6b4e3d; text-decoration: none;">SHOPEE</a></li>
+                            <li><a href="#" style="color: #6b4e3d; text-decoration: none;">TOKOPEDIA</a></li>
                         </ul>
                     </div>
                     <div class="col-md-6 text-center text-md-start">
                         <ul class="list-unstyled small mb-0">
-                            <li><a href="#" class="text-light text-decoration-none">OUR STORY</a></li>
-                            <li><a href="#" class="text-light text-decoration-none">E-CATALOG</a></li>
-                            <li><a href="#" class="text-light text-decoration-none">CONTACT US</a></li>
+                            <li><a href="#" style="color: #6b4e3d; text-decoration-none">OUR STORY</a></li>
+                            <li><a href="#" style="color: #6b4e3d; text-decoration-none">E-CATALOG</a></li>
+                            <li><a href="#" style="color: #6b4e3d; text-decoration-none">CONTACT US</a></li>
                         </ul>
                     </div>
                 </div>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('/product', function () {
+    return view('product');
+});
+
+Route::get('/store', function () {
+    return view('store');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
