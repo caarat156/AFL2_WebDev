93ef5051ae318c289a9ae6ab0249d9ac88629340
caarat156 <amaylinda@student.ciputra.ac.id>
2025-10-18 16:50:40 +0700

    home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,11 +2,15 @@
 
 @section('title', 'Home Page')
 
+@push('styles')
+<link rel="stylesheet" href="{{ asset('css/home.css') }}">
+@endpush
+
 @section('content')
 <div class="hero-section">
     <img src="{{ asset('images/welcome2.jpg') }}" alt="Candle Image" class="hero-image">
-    <div class="hero-text">
-        <h1>Fall, Redefined in Clean Luxury.</h1>
+    <div class="hero-overlay">
+        <h1 class="hero-text">Fall, Redefined in Clean Luxury.</h1>
     </div>
 </div>
 @endsection
